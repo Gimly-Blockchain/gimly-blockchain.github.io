61e37204270249acc0c45b0ebb4392d0fe4510c4
Charlie Fontana <cfontana0@gmail.com>
2023-07-06 13:02:38 +0100

    Deploy website - based on 93fadfd693f4988f2876a28665ff1636804f02f9

--- auth.js
+++ auth.js
@@ -1,14 +1,20 @@
 const observeUrlChange = () => {
-    let oldHref = document.location.href;
-    const body = document.querySelector("body");
-    const observer = new MutationObserver(mutations => {
-      if (oldHref !== document.location.href) {
-        oldHref = document.location.href;
-        console.log(localStorage.getItem('secret'),document.location.href);
+  let oldHref = document.location.href
+  const body = document.querySelector('body')
+  const observer = new MutationObserver((mutations) => {
+    if (oldHref !== document.location.href) {
+      oldHref = document.location.href
+      console.log('Hi there!', document.location.href);
+      if (document.location.href !== '/' && localStorage.getItem('secret') !== 'gimlyrocks') {
+        document.location.href = '/'
       }
-    });
-    observer.observe(body, { childList: true, subtree: true });
-  };
-  
-  window.onload = observeUrlChange;
-  console.log('inicie observeUrlChange');
+    }
+  })
+  observer.observe(body, { childList: true, subtree: true })
+}
+
+window.onload = observeUrlChange
+console.log(document.location.href);
+if (document.location.href !== '/' && localStorage.getItem('secret') !== 'gimlyrocks') {
+  document.location.href = '/'
+}
