c9420fd0c7f713f3112e1598fb4bddfbdcb630fa
Charlie Fontana <cfontana0@gmail.com>
2023-07-06 12:31:09 +0100

    Deploy website - based on 93fadfd693f4988f2876a28665ff1636804f02f9

--- auth.js
+++ auth.js
@@ -1,3 +1,14 @@
-window.addEventListener('locationchange', function () {
-    console.log('location changed!');
-});
+const observeUrlChange = () => {
+    let oldHref = document.location.href;
+    const body = document.querySelector("body");
+    const observer = new MutationObserver(mutations => {
+      if (oldHref !== document.location.href) {
+        oldHref = document.location.href;
+        alert('fuck off my friend')
+      }
+    });
+    observer.observe(body, { childList: true, subtree: true });
+  };
+  
+  window.onload = observeUrlChange;
+  console.log('inicie observeUrlChange');
